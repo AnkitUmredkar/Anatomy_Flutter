b3916e9f160ea51047c692f6d16a555d6987f460
AnkitUmredkar <149374001+AnkitUmredkar@users.noreply.github.com>
2024-03-15 18:12:29 +0530

    Update flutter_app.dart

--- lib/flutter_app.dart
+++ lib/flutter_app.dart
@@ -36,25 +36,6 @@ class MyApp extends StatelessWidget {
           ),
         ),
       ),
-      =======
-    return const MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: Scaffold(
-    backgroundColor: Colors.black,
-    body: Center(
-    child: Text('Hello\n\n\n\n\nDart\n\n\n\n\nFlutter',
-    style : TextStyle(
-    fontSize: 35,
-    fontWeight: FontWeight.bold,
-    color: Colors.red,
-    decoration: TextDecoration.underline,
-    decorationColor: Colors.amberAccent,
-      letterSpacing: 1.2,
-    ),
-    ),
-    ),
-    ),
-    >>>>>>> github/main
     );
   }
 }
