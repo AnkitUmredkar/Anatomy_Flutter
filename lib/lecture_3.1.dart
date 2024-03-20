c1e36ffe4c85470e15b62a40934a96128b7ac4af
Ankit <ankitumredkar77@gmail.com>
2024-03-20 16:16:48 +0530

    20-03-2024

---
+++ lib/lecture_3.1.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          toolbarHeight: 70,
+          backgroundColor: Colors.amber,
+          leading: const Icon(
+            Icons.menu,
+          ),
+          title: const Text('Flutter App',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25)),
+          centerTitle: true,
+        ),
+        body: const Center(
+          child: Text(
+            'Red & White',
+            style: TextStyle(
+              color: Colors.red,
+              fontSize: 40,
+              fontWeight: FontWeight.w500,
+              decoration: TextDecoration.underline,
+              decorationStyle: TextDecorationStyle.double,
+              decorationColor: Colors.amber,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/lecture_3.2.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          leading: const Icon(
+            Icons.menu,
+            color: Colors.white,
+          ),
+          toolbarHeight: 70,
+          backgroundColor: Colors.redAccent,
+          title: const Text('My RNW',
+              style: TextStyle(
+                  fontWeight: FontWeight.w500,
+                  fontSize: 24,
+                  color: Colors.white)),
+          centerTitle: true,
+        ),
+        body: const Center(
+          child: Text.rich(
+            TextSpan(
+              children: [
+                TextSpan(
+                  text: 'Red & White',
+                  style: TextStyle(
+                    color: Colors.red,
+                    fontSize: 60,
+                    fontWeight: FontWeight.bold,
+                    decoration: TextDecoration.underline,
+                    decorationColor: Colors.redAccent,
+                  ),
+                ),
+                TextSpan(
+                  text: '\n Multimedia Education',
+                  style: TextStyle(
+                    color: Colors.red,
+                    fontSize: 32,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                TextSpan(
+                  text: '\nShapping "skills" for "scalling" higher...!!',
+                  style: TextStyle(
+                    color: Colors.red,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
